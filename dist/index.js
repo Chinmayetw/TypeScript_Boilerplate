055c8e20ba559b3d583d66536347b2b8a60d0b43
Chinmay Mishra <chinmayetw@gmail.com>
2023-09-07 11:41:24 +0530

    REST APT implementation

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,22 @@
 "use strict";
-let msg = "Welcome to typescript";
-console.log(msg);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const config_1 = require("./config");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.listen(config_1.PORT, () => {
+    console.log(`🌐 \x1b[1;46mAPI listening\x1b[0m : \x1b[36mhttp://localhost:${config_1.PORT}\x1b[0m`);
+});
+app.get("/", (req, res) => {
+    const data = {
+        row: 'column'
+    };
+    res.json(data);
+});
+app.post("/send", (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-let msg: string = "Welcome to typescript"
+import express, {Request, Response} from 'express'
+import { PORT } from './config'
 
-console.log(msg)
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT,()=>{
+    console.log(`🌐 \x1b[1;46mAPI listening\x1b[0m : \x1b[36mhttp://localhost:${PORT}\x1b[0m`);
+})
+
+app.get("/", (req: Request, res: Response) => {
+    const data = {
+        row: 'column'
+    }
+    res.json(data)
+})
+
+app.post("/send", (req: Request, res: Response) => {
+    console.log(req.body)
+
+    res.sendStatus(200)
+})
